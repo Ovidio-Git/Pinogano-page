83d443aeb02147d082626a5a83cac712ef0f1778
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-04-08 09:36:02 -0500

    changes static and template

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask 
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hellou bro'
+    return render_template('index.html')
+
+
+@app.route('/ejemplo')
+def ejemplo():
+    return render_template('ejemplo.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
