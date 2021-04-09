df62aca2a4cd5d64312836e766ccec0910dbb14b
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-04-09 09:38:19 -0500

    changes files

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def index():
 
 @app.route('/chivos', methods=['GET','POST'])
 def chivos():
-    # if request.methods == 'POST':
-    #     numero = request.form['name']
-    #     print(numero)
-
-
-
+    if request.method == 'POST':
+        vector = request.form.get('vector_file')
+        start  = request.form.get('value_start')
+        finish = request.form.get('value_finish')
+        print(vector)
+        print(start)
+        print(finish)
+     
+        
     return render_template('chivos.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
