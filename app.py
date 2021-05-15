bc0843d3203e415f70c2f78a054d3579aee6e2b3
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-05-15 18:40:08 -0500

    testing with hi

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def Home():
 @app.route('/metrics/<data>', methods=['POST'])
 def esp(data):
     # DATABASE QUERIES
-    return data
+    return "hola"
 
 if __name__ == '__main__':
     #app.run("0.0.0.0",debug=False)
